ccf39bb452af4f8f96a01c63fefc1e7e53efdc74
mitk13@gmail.com <318229366Me>
2019-10-24 16:46:03 +0300

    http server with post

---
+++ server/server.pl
@@ -0,0 +1,19 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_server_files)).
+:- use_module(library(http/http_files)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/http_header)).
+:- http_handler(root(.),handle_request,[]).
+
+start:-
+   http_server(http_dispatch,[port(3000)]).
+
+handle_request(Request) :-
+    http_read_json(Request, DictIn,[json_object(term)]),
+    handle_json(DictIn,Response),
+    reply_json(Response).
+
+handle_json(Json,Response):-
+    format(user_output,"Received Play~n",[]),
+    Response=Json.
