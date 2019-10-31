566225e078ab8c77bd99aee4bfbdf8dcecf82de3
mitk13@gmail.com <318229366Me>
2019-10-31 20:15:28 +0200

    testing + changes

--- alphabeta/possible_states.pl
+++ alphabeta/possible_states.pl
@@ -33,16 +33,21 @@ movePiece(Piece,game(Pieces,settings(Level,Rows)),game(NewPieces,settings(Level,
  * */
 canMoveTo(Piece,SubPieces,Rows,NewPieces):-
     newIndex(Piece,Rows,piece(I,J,Color,IsQ)), % new indexes for the piece
-    ( member(piece(I,J,_,_),SubPieces),!, % if occupied then eat
+    ( member(piece(I,J,_,_),SubPieces), % if occupied then eat
     eat(Piece,piece(I,J,Color,IsQ),SubPieces,Rows,NewPieces); % eat the peice and update
-    NewPieces = [piece(I,J,Color,IsQ)|SubPieces]). % if empty place then procees
+    not(member(piece(I,J,_,_),SubPieces)), NewPieces = [piece(I,J,Color,IsQ)|SubPieces]). % if empty place then procees
 
 /**
  * new possible indexes to walk to
  * */    
-newIndex(piece(I,J,Color,false),Row,piece(NewI,NewJ,Color,false)):-
-    NewI is I + 1, (NewJ is J+1 ; NewJ is J-1), % progress the rows and move left or right collumn
+newIndex(piece(I,J,Color,IsQ),Row,piece(NewI,NewJ,Color,IsQ)):-
+    newI(piece(I,J,Color,IsQ),NewI), (NewJ is J+1 ; NewJ is J-1), % progress the rows and move left or right collumn
     inRange(NewI,Row), inRange(NewJ,Row). % check if the new piece is in range
+
+
+newI(piece(I,_,_,true),NewI):-  NewI is I-1; NewI is I+1.
+newI(piece(I,_,white,false),NewI):-   NewI is I+1.
+newI(piece(I,_,black,false),NewI):-   NewI is I-1.
 /**
  * if the peices is on the board
  * */
@@ -63,11 +68,13 @@ eat(OldPiece,NewPiece, SubPieces, Rows, EatenList):-
     (EatenList = [AfterEatPiece|EatenList1]; % eat once
     % check if can eat again
     (newIndex(AfterEatPiece,Rows,piece(NewI,NewJ,Color,IsQ)); % new indexes for the piece
-    BackI is EatI-2, newIndex(piece(BackI,EatJ,Color,IsQ),Rows,piece(NewI,NewJ,Color,IsQ))), % eat backwards, move the I index back twice and use forward move to mock backward move
+    drawBack(piece(EatI,_,Color,_),BackI), newIndex(piece(BackI,EatJ,Color,IsQ),Rows,piece(NewI,NewJ,Color,IsQ))), % eat backwards, move the I index back twice and use forward move to mock backward move
     member(piece(NewI,NewJ,_,_),EatenList1),!, % if occupied then eat
     eat(AfterEatPiece,piece(NewI,NewJ,Color,IsQ),EatenList1,Rows,EatenList)). % check if can eat again
 
-
+drawBack(piece(I,_,Color,_),NewI):-
+    Color=black,!, newI(piece(I,_,white,_),NewI1),newI(piece(NewI1,_,white,_),NewI);
+    newI(piece(I,_,black,_),NewI1),newI(piece(NewI1,_,black,_),NewI).
 /**
  * in case of eating
  * compute the diagonal path of the piece after eating a rival
