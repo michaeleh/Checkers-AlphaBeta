18572902f5d0cf1cb4198ea40a1f2940193c3590
ilan levi <ilan_levi@hotmail.com>
2019-11-05 23:06:39 +0200

    removed todo

--- alphabeta/heuristic_static_values.pl
+++ alphabeta/heuristic_static_values.pl
@@ -4,17 +4,15 @@
  * using a heuristic predicates
  * */
 :- consult('heuristic_calc.pl'). % import possible states impl.
-% TODO, check max and min values of the hueristic function to use as parameters in alpha beta
 ?- assert(saved_value(0,0)). % dummy value for compiler.
 
- % TODO: impl heuristic_static_values for start/middle and endgame.
 staticval(Pos, Val):-
-    % value us already in db
-    retract(saved_value(Pos, Val)), ! % remove from db
-    assert(saved_value(Pos, Val)).  % add it again 
+    % value is already in db
+    saved_value(Pos,Val).
 
 staticval(Pos, Val):-
-    heuristic_calc_for_pos(Pos, Val), !,
-    assert(saved_value(Pos, Val)).
+    % (value isn't in db)
+    heuristic_calc_for_pos(Pos, Val), !, % calc value
+    assert(saved_value(Pos, Val)). % add it to db
 
         
